92414286600d5191d0dc061d2c35b6c6a84cdaea
tan_nguyen <nguyentrongtan20032003@gmail.com>
2024-10-01 16:54:54 +0700

    savedPosts

--- resources/views/saved_posts/index.blade.php
+++ resources/views/saved_posts/index.blade.php
@@ -1,66 +1,146 @@
-<!-- resources/views/home.blade.php -->
+@extends('layouts.index')
 
-@extends('layouts.index') <!-- Kế thừa từ layout 'app' -->
+@section('title', 'Batdongsan')
+
 @section('content')
-<div class="saved-posts">
-    <h1>Các Bài Viết Đã Lưu</h1>
 
-    @if($savedPosts->isEmpty())
-    <p>Không có bài viết nào được lưu.</p>
-    @else
-    <table>
-        <thead>
-            <tr>
-                <th>Tên Bài Viết</th>
-                <th>Ngày Lưu</th>
-                <th>Chức Năng</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($savedPosts as $savedPost)
-            <a href="ban-nha-mau-luong-o-to-qua-cua-kinh-doanh-dt-35m-x-5t-64-ty-r117358" class="card-cm" title="BÁN NHÀ MẬU LƯƠNG Ô TÔ QUA CỬA - KINH DOANH DT 35M x 5T 6.4 tỷ">
-                <div class="card-image">
-                    <img
-                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjWLKu/z8ABrwC4TW+tGIAAAAASUVORK5CYII="
-                        lazy-src="https://media.batdongsan.vn/crop/240x206/posts/117358_66c9eb0d18a73.jpg"
-                        alt="BÁN NHÀ MẬU LƯƠNG Ô TÔ QUA CỬA - KINH DOANH DT 35M x 5T 6.4 tỷ" />
-                    <div class="see">
-                        <div><img src="assets/img/camera.png" /></div>
-                        <div>5</div>
-                    </div>
-                </div>
-                <div class="card-content">
-                    <h3 class="title">
-                        BÁN NHÀ MẬU LƯƠNG Ô TÔ QUA CỬA - KINH DOANH DT 35M x 5T 6.4 tỷ
-                    </h3>
-                    <div>
-                        <div class="description">Hà Đông , Hà Nội</div>
-                        <div class="description-tag">
-                            <div class="description-item">35 m2</div>
-                            <div class="description-item">3 Phòng ngủ</div>
-                            <div class="description-item">3 WC</div>
+
+<h1 style="text-align: center; margin-top: 20px; text-decoration: underline;">Danh sách tin lưu</h1>
+
+<div class="chuyenmuc container mt-4" id="danhmuc">
+
+    <div class="row mt-sm-4 mt-3">
+        <div class="col-md-9 col-sm-12 left-element">
+
+            <div class="gap-24 d-flex flex-column card-container">
+
+                @foreach($savedPosts as $savedPost)
+
+                <a href="/real-state/{{ $savedPost->post->id }}" class="card-cm" title="{{$savedPost->post->title}}">
+                    <div class="card-image">
+                        <img
+                            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjWLKu/z8ABrwC4TW+tGIAAAAASUVORK5CYII="
+                            lazy-src="{{$savedPost->post->images[0]->image_url}}"
+                            alt="{{$savedPost->post->title}}" />
+                        <div class="see">
+                            <div><img src="assets/img/camera.png" /></div>
+                            <div>5</div>
                         </div>
                     </div>
-                    <div class="content-footer">
+                    <div class="card-content">
+                        <h3 class="title">
+                            {{$savedPost->post->title}}
+                        </h3>
                         <div>
-                            <div class="price">
-                                6 tỷ 400 triệu
-                            </div>
-
-                            <div class="time">
-                                2 giờ trước
+                            <div class="description">{{$savedPost->post->huyen->name}} , {{$savedPost->post->province->name}}</div>
+                            <div class="description-tag">
+                                <div class="description-item">{{$savedPost->post->area}} m2</div>
+                                <div class="description-item">{{$savedPost->post->num_of_bedrooms}} Phòng ngủ</div>
+                                <div class="description-item">{{$savedPost->post->num_of_bathrooms}} WC</div>
                             </div>
                         </div>
-                        <div class="heart">
-                            <span class="btn-save js-btn-save add-like" post_id="117358" title=" Lưu tin này " data-original-title="Tin đã lưu"><i class="icon heart margin-right-0"></i></span>
+                        <div class="content-footer">
+                            <div>
+                                <div class="price">
+                                    @php
+                                    $number = $savedPost->post->price; // Giá trị từ bài đăng
+                                    $billion = floor($number / 1000000000); // Lấy phần tỷ
+                                    $million = floor(($number % 1000000000) / 1000000); // Lấy phần triệu còn lại sau khi chia tỷ
+                                    $thousands = floor(($number % 1000000) / 1000); // Lấy phần nghìn còn lại sau khi chia triệu
+                                    $hundreds = $number % 1000; // Lấy phần trăm nghìn trở xuống
+                                    @endphp
+
+                                    @if ($billion > 0)
+                                    {{ $billion }} tỷ
+                                    @endif
+
+                                    @if ($million > 0)
+                                    {{ $million }} triệu
+                                    @endif
+
+                                    @if ($thousands > 0)
+                                    {{ $thousands }} nghìn
+                                    @endif
+
+                                    @if ($hundreds > 0)
+                                    {{ $hundreds }} đồng
+                                    @endif
+
+                                </div>
+
+                                <div class="time">
+                                    {{$savedPost->post->created_at}}
+                                </div>
+                            </div>
+                            <form action="{{Route('delete.saved.post', ['id'=>$savedPost->post->id])}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="heart btn_parent_btn-save" style="display: inline-block; outline: none; border: none; background-color: #fff;">
+                                    <!-- có class saved là đã lưu -->
+                                    <span style="padding: 5px ; display: inline-block;" class="saved btn-save js-btn-save add-like" post_id="117358" title=" Lưu tin này " data-original-title="Tin đã lưu"><i class="icon heart margin-right-0"></i></span>
+                                </button>
+
+                            </form>
+
                         </div>
                     </div>
-                </div>
-            </a>
+                </a>
+                @endforeach
 
-            @endforeach
-        </tbody>
-    </table>
-    @endif
+
+                {{-- <div class="paging">
+                    <nav aria-label=" Page navigation example">
+                        <ul class="pagination">
+                            <div class="paging">
+                                <ul class="pagination">
+                                    <li class="disabled"><span>&laquo;</span></li>
+
+                                    <li class="active"><span>1</span></li>
+                                    <li><a href="ban-nha/p2">2</a></li>
+                                    <li><a href="ban-nha/p3">3</a></li>
+                                    <li><a href="ban-nha/p4">4</a></li>
+                                    <li><a href="ban-nha/p5">5</a></li>
+                                    <li><a href="ban-nha/p6">6</a></li>
+                                    <li><a href="ban-nha/p7">7</a></li>
+                                    <li><a href="ban-nha/p8">8</a></li>
+                                    <li><a href="ban-nha/p9">9</a></li>
+                                    <li><a href="ban-nha/p10">10</a></li>
+
+                                    <li class="disabled"><span>...</span></li>
+
+                                    <li><a href="ban-nha/p732">732</a></li>
+                                    <li><a href="ban-nha/p733">733</a></li>
+
+                                    <li><a href="ban-nha/p2" rel="next">&raquo;</a></li>
+                                </ul>
+                            </div>
+                        </ul>
+                    </nav>
+                </div> --}}
+            </div>
+
+            <div class="description-text mt-4 pb-4">
+                <p class="mb-3">
+
+                </p>
+            </div>
+        </div>
+
+
+    </div>
 </div>
+
+
+
 @endsection
+
+<script>
+    // let btnTim = document.querySelector(".btn-save.js-btn-save.add-like");
+    let buttonParentBtnTim = document.querySelectorAll(".btn_parent_btn-save");
+    buttonParentBtnTim.forEach((item) => {
+        item.onclick(() => {
+            console.log("hello");
+
+        });
+    });
+</script>
