586b4ce5d1fc822d459e8c16f8a38795688b3caf
trantu1243 <trantu1242003@gmail.com>
2024-10-06 19:55:10 +0700

    .

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -352,7 +352,7 @@
                                 <select
                                     id="select-huyen"
                                     title="Quận/Huyện"
-                                    name="DistrictIdsAsString"
+                                    name="DistrictIdsAsString[]"
                                     class="selectpicker filter-select-boostrap"
                                     data-live-search="true"
                                     data-size="2000"
